daa6be7144b94842424019b6d73e29798896fa28
chooblarin <choo.bla.rin@gmail.com>
2017-07-08 12:09:23 +0900

    Hello web audio from p5js-sound

--- app/index.js
+++ app/index.js
@@ -1,14 +1,32 @@
 console.log('hello')
 
 import p5 from 'p5'
+import 'p5/lib/addons/p5.sound'
 
-const sketch = function(p) {
+const sketch = function (p) {
 
-  p.setup = function() {
-    p.createCanvas(p.windowWidth, p.windowHeight)
+  let p5canvas
+  let sampleSound
+
+  p.preload = function () {
+    sampleSound = p.loadSound('assets/sample.mp3')
   }
 
-  p.draw = function() {
+  p.setup = function () {
+    p5canvas = p.createCanvas(p.windowWidth, p.windowHeight)
+    p5canvas.mouseClicked(() => {
+      if (sampleSound.isPlaying()) {
+        sampleSound.pause()
+      } else {
+        sampleSound.play()
+      }
+    })
+
+    sampleSound.setVolume(0.1)
+    sampleSound.play()
+  }
+
+  p.draw = function () {
     p.fill(0)
     p.ellipse(50, 50, 80, 80)
   }
