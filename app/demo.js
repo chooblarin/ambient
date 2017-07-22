f054bcf3886f44a1eafdc6556fd9f084ac1389d7
chooblarin <choo.bla.rin@gmail.com>
2017-07-22 13:13:09 +0900

    Select colors

--- app/demo.js
+++ app/demo.js
@@ -79,7 +79,7 @@ export function sketch(p) {
       const prop = pprops[r]
       const count = prop.count
       for (let i = 0; i < count; i += 1) {
-        const col = randomColor()
+        const col = randomSelectedColor()
         const size = prop.size
         const particle = spawnParticle(size, col)
         particles.push(particle)
@@ -104,4 +104,38 @@ export function sketch(p) {
     const b = 255 * Math.random()
     return p.color(r, g, b)
   }
+
+  let randomSelectedColor = () => {
+    const c = colors[Math.floor(Math.random() * colors.length)]
+    return p.color(c)
+  }
+
+  const colors = [
+    'rgb( 25,  68, 133)',
+    'rgb( 30,  53, 141)',
+    'rgb( 69,   2, 115)',
+    'rgb(109,  18,  78)',
+    'rgb(102,  22,  90)',
+    'rgb( 99,  57, 147)',
+    'rgb( 193,  62, 254)',
+    'rgb(242, 159, 183)',
+    'rgb(221, 115, 167)',
+    'rgb(218,   0, 109)',
+    'rgb(225,  51,  35)',
+    'rgb(226,  77,  76)',
+    'rgb(238, 140,  38)',
+    'rgb(249, 196,   9)',
+    'rgb(243, 213,  34)',
+    'rgb(253, 243,  10)',
+    'rgb(168, 204,   7)',
+    'rgb( 33, 178,  80)',
+    'rgb( 30, 135,  40)',
+    'rgb( 18, 143, 150)',
+    'rgb(105, 210, 231)',
+    'rgb(167, 219, 216)',
+    'rgb(224, 228, 204)',
+    'rgb( 26, 146, 166)',
+    'rgb( 17, 140, 226)',
+    'rgb(  0,  71, 255)'
+  ]
 }
